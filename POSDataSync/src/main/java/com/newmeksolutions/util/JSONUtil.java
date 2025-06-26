40ccc7d1d97a1cc25d16fd008da3c82b899bcd61
aditivarma9 <aditivarma2k5@gmail.com>
2025-06-26 14:54:21 +0530

    Changes made by Aditi

--- POSDataSync/src/main/java/com/newmeksolutions/util/JSONUtil.java
+++ POSDataSync/src/main/java/com/newmeksolutions/util/JSONUtil.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONUtil 
 {
+
+	// This code Converts the Any class object into JSON and Returns the JSON Value of the Object //
 	private static final ObjectMapper mapper = new ObjectMapper();
 
 	    public static String toJSON(Object obj) throws Exception 
